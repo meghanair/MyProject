1732ff743353f0879d0dcef142d581fcbe5103b3
meghanair <manair712@gmail.com>
2017-01-18 23:03:49 -0600

    From scratch

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myProjectApp
+ * @description
+ * # myProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myProjectApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
